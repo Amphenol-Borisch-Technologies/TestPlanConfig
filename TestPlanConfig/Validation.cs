21830239982ee30c9921f63de41f93affc08d450
Phillip Smelt <phils@borisch.com>
2024-11-19 11:47:18 -0500

    11/19/24

--- TestPlanConfig/Validation.cs
+++ TestPlanConfig/Validation.cs
@@ -2,7 +2,7 @@
 using System.Xml;
 using System.Xml.Schema;
 
-class Validator {
+static class Validator {
     static Boolean xmlValid = true;
     static void Main() {
         XmlSchemaSet schemaSet = new XmlSchemaSet();
@@ -12,20 +12,25 @@ class Validator {
             Schemas = schemaSet
         };
         settings.ValidationEventHandler += ValidationCallback;
-
-        using (XmlReader reader = XmlReader.Create(@"C:\Users\phils\source\repos\TestPlanConfig\TestPlanConfig\T10.xml", settings)) {
-            while (reader.Read()) { }
+        try {
+            using (XmlReader reader = XmlReader.Create(@"C:\Users\phils\source\repos\TestPlanConfig\TestPlanConfig\T10.xml", settings)) { while (reader.Read()) { } }
+        } catch (Exception ex) {
+            xmlValid = false;
+            Console.WriteLine($"Error: {ex.Message}{Environment.NewLine}");
         }
 
-        if (xmlValid) Console.WriteLine("XML document is valid.");
+        if (xmlValid) Console.WriteLine($"XML document is valid.{Environment.NewLine}");
+        else Console.WriteLine($"XML document is not valid.{Environment.NewLine}");
     }
 
-    static void ValidationCallback(Object sender, ValidationEventArgs e) {
+    static void ValidationCallback(Object sender, ValidationEventArgs vea) {
         xmlValid = false;
-        if (e.Severity == XmlSeverityType.Warning) {
-            Console.WriteLine($"Warning: {e.Message}");
-        } else if (e.Severity == XmlSeverityType.Error) {
-            Console.WriteLine($"Error: {e.Message}");
-        }
+        Console.WriteLine($"Source             : {vea.Exception.Source}");
+        Console.WriteLine($"SourceUri          : {vea.Exception.SourceUri}");
+        Console.WriteLine($"LineNumber         : {vea.Exception.LineNumber}");
+        Console.WriteLine($"LinePosition       : {vea.Exception.LinePosition}");
+        Console.WriteLine($"SourceSchemaObject : {vea.Exception.SourceSchemaObject}");
+        Console.WriteLine($"Severity           : {vea.Severity}");
+        Console.WriteLine($"Message            : {vea.Message}{Environment.NewLine}");
     }
 }
