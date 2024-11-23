326fdd302fabff1e1b9565ff0feaf5e14c8be2a8
Phillip Smelt <phils@borisch.com>
2024-11-23 13:40:09 -0500

    11/22/24

--- TestPlanConfig/Validator.cs
+++ TestPlanConfig/Validator.cs
@@ -25,19 +25,37 @@ namespace TestSequencer {
                     Double low, high;
                     while (reader.Read()) {
                         if (reader.NodeType == XmlNodeType.Element && reader.Name == "MI") {
-                            // NOTE: This if block required because Microsoft Visual Studio only supports XML Schema 1.0.
-                            // - If Visual Studio supported XSD 1.1, then <xs:assert test="@Low le @High"/>
-                            //   would obviate this if block.
-                            // - XML Liquid Studio Community Edition supports XML Schema 1.1.
-                            // - Microsoft Visual Studio only supports XML schema 1.0.
-                            // - Microsoft Visual Studio Code with Red Hat's XML extension only supports XML schema 1.0.
-                            //   - Tried several other VS Code extensions, but they didn't support XML schema 1.1 either.
-                            // - XML Notepad only supports only supports XML schema 1.0.
+                            // NOTE: This if block required because Microsoft's Visual Studio only supports XML Schema 1.0.
+                            // - If Visual Studio supported XSD 1.1, then <xs:assert test="@Low le @High"/>  would obviate this block.
+                            // - Below NOTES compare some mainstream XML editing options.
+                            //
+                            // NOTE: XML Liquid Studio Community Edition supports XML Schema 1.1.
+                            // - Liquid Studio is a powerful but complex external XML editor.
+                            // - It's co$t free and it's license permits commericial usage.
+                            // - Confirmed it detects Low > High occurences via <xs:assert test="@Low le @High"/>.
+                            // - Chose to not utilize Liquid Studio because it adds too much complexity at this time.
+                            //
+                            // NOTE: XML Notepad supports XML Schema 1.0.
+                            // - XML Notepad is a powerful but simple external XML editor.
+                            // - It's co$t free and it's license permits commericial usage.
+                            //
+                            // NOTE: Visual Studio Code with Red Hat's XML extension supports XML Schema 1.0.
+                            // - VS Code is a powerful but complex external multi-purpose editor.
+                            // - It's co$t free and it's license permits commericial usage.
+                            //   - Red Hat's XML extension provides XML Schema 1.0 support.
+                            //   - Tried several other provider's XML extensions, but none supported XML schema 1.1.
+                            //   - As a multi-purpose editor, can develop C# .Net applications.  Plus many other languages.
+                            //
+                            // NOTE: Visual Studio's supports XML Schema 1.0.
+                            // - Visual Studio's integrated XML editor is powerful but complex.
+                            //   - Being integrated with Visual Studio is incredibly convenient however.
+                            //   - Visual Studio isn't co$t free, but permits commercial use.
+                            //   - As a multi-purpose editor, can develop C# .Net applications.  Plus many other languages.
                             low = Double.Parse(reader.GetAttribute("Low"));
                             high = Double.Parse(reader.GetAttribute("High"));
                             if (low > high) {
                                 xmlValid = false;
-                                messages.AppendLine($"Error:");
+                                messages.AppendLine($"MethodInterval's Low > High:");
                                 messages.AppendLine($"  Line Number   : {(reader as IXmlLineInfo).LineNumber}");
                                 messages.AppendLine($"  Line Position : {(reader as IXmlLineInfo).LinePosition}");
                                 messages.AppendLine($"  Node Type     : {reader.NodeType}");
@@ -45,15 +63,15 @@ namespace TestSequencer {
                                 messages.AppendLine($"  Method        : {reader.GetAttribute("Method")}");
                                 messages.AppendLine($"  Low           : {reader.GetAttribute("Low")}");
                                 messages.AppendLine($"  High          : {reader.GetAttribute("High")}");
-                                messages.AppendLine($"  XML           : {reader.ReadOuterXml()}");
-                                messages.AppendLine($"  Message       : MethodInterval's Low > High.{Environment.NewLine}");
+                                messages.AppendLine($"  XML           : {reader.ReadOuterXml()}{Environment.NewLine}");
                             }
                         }
                     }
                 }
             } catch (Exception ex) {
                 xmlValid = false;
-                messages.AppendLine($"Error:{Environment.NewLine}{ex.Message}{Environment.NewLine}");
+                messages.AppendLine($"Exception:");
+                messages.AppendLine($"  Exception     : {ex.Message}{Environment.NewLine}");
             }
 
             if (!xmlValid) {
@@ -64,9 +82,10 @@ namespace TestSequencer {
 
         private static void ValidationCallback(Object sender, ValidationEventArgs vea) {
             xmlValid = false;
-            messages.AppendLine($"Error:");
+            messages.AppendLine($"Validation Event:");
             messages.AppendLine($"  Line Number   : {vea.Exception.LineNumber}");
             messages.AppendLine($"  Line Position : {vea.Exception.LinePosition}");
+            messages.AppendLine($"  Severity      : {vea.Severity}");
             messages.AppendLine($"  Node Type     : {reader.NodeType}");
             messages.AppendLine($"  Description   : {reader.GetAttribute("Description")}");
             messages.AppendLine($"  XML           : {reader.ReadOuterXml()}");
